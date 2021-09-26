2ab872675490f4834e4763511ebfb58c6a07d7af
Naked-Snake04 <joKer041200saV@gmail.com>
2021-09-26 03:22:49 +0300

    Changed main

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'mainpage.dart';
 
 /*
 void main() => runApp(const MyApp());
