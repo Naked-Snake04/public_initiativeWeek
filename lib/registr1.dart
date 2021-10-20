bc3f804a4e46ba17b9806b91b135f23378c3f269
Naked-Snake04 <joKer041200saV@gmail.com>
2021-10-20 21:00:30 +0300

    Added ignore

--- lib/registr1.dart
+++ lib/registr1.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: camel_case_types, prefer_const_constructors_in_immutables, prefer_const_constructors
+// ignore_for_file: camel_case_types, prefer_const_constructors_in_immutables, prefer_const_constructors, constant_identifier_names
 
 import 'package:flutter/material.dart';
 import 'package:adobe_xd/pinned.dart';
